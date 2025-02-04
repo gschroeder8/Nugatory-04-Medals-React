8e918f71f861f55e7be67c526f6dd3eb64bbea37
gschroeder8 <gschroeder8@my.wctc.edu>
2025-02-04 17:41:50 -0600

    Submission

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Nugatory-04 -Medals-React',
+  base: '/Nugatory-04-Medals-React',
   plugins: [react()],
 })
